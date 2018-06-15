26c5cc13a846800f9ffade45aed6f2e64bb065f7
sheepdog-mcfry <sheepdog.mcfry@gmail.com>
2018-06-15 14:45:12 +0200

    Initial Commit

---
+++ guibr_vars.lua
@@ -0,0 +1,6 @@
+return {{[ [[showaffs]] ] = true,[ [[fadewarn]] ] = true,[ [[on]] ] = [[green]],[ [[gag]] ] = true,[ [[y]] ] = 100,[ [[x]] ] = 500,[ [[style]] ] = [[border-width: 2px;
+						border-color: #400020;
+						border-style: groove;
+						border-radius: 1;
+						]],[ [[w]] ] = 100,[ [[h]] ] = 20,[ [[off]] ] = [[grey]],[ [[bg]] ] = [[black]],[ [[fg]] ] = [[tomato]],},
+}
